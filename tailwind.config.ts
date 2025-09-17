7ecd87c8744f94c3865cc0507274c4282e04dfe8
raydatorre <lykewer@gmail.com>
2025-09-17 10:14:51 -0300

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,16 +4,17 @@ const config: Config = {
   content: ["./src/app/**/*.{ts,tsx}","./src/components/**/*.{ts,tsx}","./src/lib/**/*.{ts,tsx}"],
   theme: {
     extend: {
-      colors: {
-        background: "rgb(var(--background))",
-        foreground: "rgb(var(--foreground))",
-        muted: "rgb(var(--muted))",
-        card: "rgb(var(--card))",
-        primary: { DEFAULT: "rgb(var(--primary))" },
-        accent: { DEFAULT: "rgb(var(--accent))" },
-      },
-      borderRadius: { xl: "1rem", "2xl": "1.25rem" },
-    },
+  colors: {
+    background: "rgb(var(--background))",
+    foreground: "rgb(var(--foreground))",
+    muted: "rgb(var(--muted))",
+    card: "rgb(var(--card))",
+    primary: { DEFAULT: "rgb(var(--primary))" },
+    accent: { DEFAULT: "rgb(var(--accent))" },
+    "muted-foreground": "rgb(var(--muted-foreground))", // << novo
+  },
+  borderRadius: { xl: "1rem", "2xl": "1.25rem" },
+},
   },
   plugins: [],
 };
